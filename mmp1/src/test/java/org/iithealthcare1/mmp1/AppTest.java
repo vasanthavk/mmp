36c108ee74ba520f604dfecf5c6174ca5513554f
vasanthavk <vasanthavk>
2021-06-04 17:35:24 -0400

    Maintaing the code in master branch

---
+++ mmp1/src/test/java/org/iithealthcare1/mmp1/AppTest.java
@@ -0,0 +1,20 @@
+package org.iithealthcare1.mmp1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
